96453cb7d236a191d06ced7aedf88c0377ff36ae
Christoph Hänisch <christoph.haenisch@rwth-aachen.de>
2025-02-14 23:31:08 +0100

    Implemented the logic for the first tab of the GUI. The tab now has a list of all the MIDI devices connected to the computer, and the user can select one ore more of them to use as the input device.

--- gui/MainWidget.py
+++ gui/MainWidget.py
@@ -15,17 +15,17 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QFrame, QGridLayout, QGroupBox,
-    QHBoxLayout, QHeaderView, QPushButton, QRadioButton,
-    QSizePolicy, QSpacerItem, QTabWidget, QTableWidget,
-    QTableWidgetItem, QTextEdit, QWidget)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QFrame, QGridLayout,
+    QGroupBox, QHBoxLayout, QHeaderView, QPushButton,
+    QRadioButton, QSizePolicy, QSpacerItem, QTabWidget,
+    QTableWidget, QTableWidgetItem, QTextEdit, QWidget)
 
 class Ui_MainWidget(object):
     def setupUi(self, MainWidget):
         if not MainWidget.objectName():
             MainWidget.setObjectName(u"MainWidget")
         MainWidget.resize(586, 392)
-        MainWidget.setMinimumSize(QSize(586, 392))
+        MainWidget.setMinimumSize(QSize(0, 0))
         self.gridLayout_2 = QGridLayout(MainWidget)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.tabWidget = QTabWidget(MainWidget)
@@ -39,50 +39,18 @@ class Ui_MainWidget(object):
         self.gridLayout = QGridLayout(self.groupBox_LocalOutputPorts)
         self.gridLayout.setObjectName(u"gridLayout")
         self.tableWidget_LocalOutputPorts = QTableWidget(self.groupBox_LocalOutputPorts)
-        if (self.tableWidget_LocalOutputPorts.columnCount() < 3):
-            self.tableWidget_LocalOutputPorts.setColumnCount(3)
+        if (self.tableWidget_LocalOutputPorts.columnCount() < 2):
+            self.tableWidget_LocalOutputPorts.setColumnCount(2)
         font = QFont()
         font.setBold(True)
         __qtablewidgetitem = QTableWidgetItem()
+        __qtablewidgetitem.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
         __qtablewidgetitem.setFont(font);
         self.tableWidget_LocalOutputPorts.setHorizontalHeaderItem(0, __qtablewidgetitem)
         __qtablewidgetitem1 = QTableWidgetItem()
         __qtablewidgetitem1.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
         __qtablewidgetitem1.setFont(font);
         self.tableWidget_LocalOutputPorts.setHorizontalHeaderItem(1, __qtablewidgetitem1)
-        __qtablewidgetitem2 = QTableWidgetItem()
-        __qtablewidgetitem2.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
-        __qtablewidgetitem2.setFont(font);
-        self.tableWidget_LocalOutputPorts.setHorizontalHeaderItem(2, __qtablewidgetitem2)
-        if (self.tableWidget_LocalOutputPorts.rowCount() < 3):
-            self.tableWidget_LocalOutputPorts.setRowCount(3)
-        __qtablewidgetitem3 = QTableWidgetItem()
-        self.tableWidget_LocalOutputPorts.setVerticalHeaderItem(0, __qtablewidgetitem3)
-        __qtablewidgetitem4 = QTableWidgetItem()
-        self.tableWidget_LocalOutputPorts.setVerticalHeaderItem(1, __qtablewidgetitem4)
-        __qtablewidgetitem5 = QTableWidgetItem()
-        self.tableWidget_LocalOutputPorts.setVerticalHeaderItem(2, __qtablewidgetitem5)
-        __qtablewidgetitem6 = QTableWidgetItem()
-        __qtablewidgetitem6.setTextAlignment(Qt.AlignCenter);
-        self.tableWidget_LocalOutputPorts.setItem(0, 0, __qtablewidgetitem6)
-        __qtablewidgetitem7 = QTableWidgetItem()
-        self.tableWidget_LocalOutputPorts.setItem(0, 1, __qtablewidgetitem7)
-        __qtablewidgetitem8 = QTableWidgetItem()
-        self.tableWidget_LocalOutputPorts.setItem(0, 2, __qtablewidgetitem8)
-        __qtablewidgetitem9 = QTableWidgetItem()
-        __qtablewidgetitem9.setTextAlignment(Qt.AlignCenter);
-        self.tableWidget_LocalOutputPorts.setItem(1, 0, __qtablewidgetitem9)
-        __qtablewidgetitem10 = QTableWidgetItem()
-        self.tableWidget_LocalOutputPorts.setItem(1, 1, __qtablewidgetitem10)
-        __qtablewidgetitem11 = QTableWidgetItem()
-        self.tableWidget_LocalOutputPorts.setItem(1, 2, __qtablewidgetitem11)
-        __qtablewidgetitem12 = QTableWidgetItem()
-        __qtablewidgetitem12.setTextAlignment(Qt.AlignCenter);
-        self.tableWidget_LocalOutputPorts.setItem(2, 0, __qtablewidgetitem12)
-        __qtablewidgetitem13 = QTableWidgetItem()
-        self.tableWidget_LocalOutputPorts.setItem(2, 1, __qtablewidgetitem13)
-        __qtablewidgetitem14 = QTableWidgetItem()
-        self.tableWidget_LocalOutputPorts.setItem(2, 2, __qtablewidgetitem14)
         self.tableWidget_LocalOutputPorts.setObjectName(u"tableWidget_LocalOutputPorts")
         palette = QPalette()
         brush = QBrush(QColor(0, 218, 105, 255))
@@ -93,9 +61,10 @@ class Ui_MainWidget(object):
         palette.setBrush(QPalette.Inactive, QPalette.Accent, brush)
         palette.setBrush(QPalette.Disabled, QPalette.Accent, brush)
         self.tableWidget_LocalOutputPorts.setPalette(palette)
+        self.tableWidget_LocalOutputPorts.setEditTriggers(QAbstractItemView.EditTrigger.CurrentChanged|QAbstractItemView.EditTrigger.DoubleClicked|QAbstractItemView.EditTrigger.EditKeyPressed|QAbstractItemView.EditTrigger.SelectedClicked)
         self.tableWidget_LocalOutputPorts.horizontalHeader().setVisible(True)
-        self.tableWidget_LocalOutputPorts.horizontalHeader().setMinimumSectionSize(25)
-        self.tableWidget_LocalOutputPorts.horizontalHeader().setDefaultSectionSize(80)
+        self.tableWidget_LocalOutputPorts.horizontalHeader().setMinimumSectionSize(100)
+        self.tableWidget_LocalOutputPorts.horizontalHeader().setDefaultSectionSize(250)
         self.tableWidget_LocalOutputPorts.horizontalHeader().setStretchLastSection(True)
         self.tableWidget_LocalOutputPorts.verticalHeader().setVisible(False)
         self.tableWidget_LocalOutputPorts.verticalHeader().setHighlightSections(False)
@@ -110,10 +79,15 @@ class Ui_MainWidget(object):
 
         self.horizontalLayout.addWidget(self.pushButton_LocalOutputPorts_SelectAll)
 
-        self.pushButton_LocalOutputPorts_UnSelectAll = QPushButton(self.groupBox_LocalOutputPorts)
-        self.pushButton_LocalOutputPorts_UnSelectAll.setObjectName(u"pushButton_LocalOutputPorts_UnSelectAll")
+        self.pushButton_LocalOutputPorts_UnselectAll = QPushButton(self.groupBox_LocalOutputPorts)
+        self.pushButton_LocalOutputPorts_UnselectAll.setObjectName(u"pushButton_LocalOutputPorts_UnselectAll")
 
-        self.horizontalLayout.addWidget(self.pushButton_LocalOutputPorts_UnSelectAll)
+        self.horizontalLayout.addWidget(self.pushButton_LocalOutputPorts_UnselectAll)
+
+        self.pushButton_LocalOutputPorts_Refresh = QPushButton(self.groupBox_LocalOutputPorts)
+        self.pushButton_LocalOutputPorts_Refresh.setObjectName(u"pushButton_LocalOutputPorts_Refresh")
+
+        self.horizontalLayout.addWidget(self.pushButton_LocalOutputPorts_Refresh)
 
         self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
@@ -155,30 +129,30 @@ class Ui_MainWidget(object):
         self.tableWidget_RTT = QTableWidget(self.groupBox_RTT)
         if (self.tableWidget_RTT.columnCount() < 5):
             self.tableWidget_RTT.setColumnCount(5)
-        __qtablewidgetitem15 = QTableWidgetItem()
-        __qtablewidgetitem15.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
-        __qtablewidgetitem15.setFont(font);
-        self.tableWidget_RTT.setHorizontalHeaderItem(0, __qtablewidgetitem15)
-        __qtablewidgetitem16 = QTableWidgetItem()
-        __qtablewidgetitem16.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
-        __qtablewidgetitem16.setFont(font);
-        self.tableWidget_RTT.setHorizontalHeaderItem(1, __qtablewidgetitem16)
-        __qtablewidgetitem17 = QTableWidgetItem()
-        __qtablewidgetitem17.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
-        __qtablewidgetitem17.setFont(font);
-        self.tableWidget_RTT.setHorizontalHeaderItem(2, __qtablewidgetitem17)
-        __qtablewidgetitem18 = QTableWidgetItem()
-        __qtablewidgetitem18.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
-        __qtablewidgetitem18.setFont(font);
-        self.tableWidget_RTT.setHorizontalHeaderItem(3, __qtablewidgetitem18)
-        __qtablewidgetitem19 = QTableWidgetItem()
-        __qtablewidgetitem19.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
-        __qtablewidgetitem19.setFont(font);
-        self.tableWidget_RTT.setHorizontalHeaderItem(4, __qtablewidgetitem19)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        __qtablewidgetitem2.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
+        __qtablewidgetitem2.setFont(font);
+        self.tableWidget_RTT.setHorizontalHeaderItem(0, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        __qtablewidgetitem3.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
+        __qtablewidgetitem3.setFont(font);
+        self.tableWidget_RTT.setHorizontalHeaderItem(1, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        __qtablewidgetitem4.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
+        __qtablewidgetitem4.setFont(font);
+        self.tableWidget_RTT.setHorizontalHeaderItem(2, __qtablewidgetitem4)
+        __qtablewidgetitem5 = QTableWidgetItem()
+        __qtablewidgetitem5.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
+        __qtablewidgetitem5.setFont(font);
+        self.tableWidget_RTT.setHorizontalHeaderItem(3, __qtablewidgetitem5)
+        __qtablewidgetitem6 = QTableWidgetItem()
+        __qtablewidgetitem6.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
+        __qtablewidgetitem6.setFont(font);
+        self.tableWidget_RTT.setHorizontalHeaderItem(4, __qtablewidgetitem6)
         if (self.tableWidget_RTT.rowCount() < 1):
             self.tableWidget_RTT.setRowCount(1)
-        __qtablewidgetitem20 = QTableWidgetItem()
-        self.tableWidget_RTT.setVerticalHeaderItem(0, __qtablewidgetitem20)
+        __qtablewidgetitem7 = QTableWidgetItem()
+        self.tableWidget_RTT.setVerticalHeaderItem(0, __qtablewidgetitem7)
         self.tableWidget_RTT.setObjectName(u"tableWidget_RTT")
         self.tableWidget_RTT.horizontalHeader().setStretchLastSection(True)
         self.tableWidget_RTT.verticalHeader().setVisible(False)
@@ -222,7 +196,7 @@ class Ui_MainWidget(object):
 
         QWidget.setTabOrder(self.tabWidget, self.tableWidget_LocalOutputPorts)
         QWidget.setTabOrder(self.tableWidget_LocalOutputPorts, self.pushButton_LocalOutputPorts_SelectAll)
-        QWidget.setTabOrder(self.pushButton_LocalOutputPorts_SelectAll, self.pushButton_LocalOutputPorts_UnSelectAll)
+        QWidget.setTabOrder(self.pushButton_LocalOutputPorts_SelectAll, self.pushButton_LocalOutputPorts_UnselectAll)
 
         self.retranslateUi(MainWidget)
 
@@ -236,60 +210,33 @@ class Ui_MainWidget(object):
         MainWidget.setWindowTitle(QCoreApplication.translate("MainWidget", u"MIDI over LAN", None))
         self.groupBox_LocalOutputPorts.setTitle(QCoreApplication.translate("MainWidget", u"Local Output Ports", None))
         ___qtablewidgetitem = self.tableWidget_LocalOutputPorts.horizontalHeaderItem(0)
-        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWidget", u"Activate", None));
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWidget", u"Device Name", None));
         ___qtablewidgetitem1 = self.tableWidget_LocalOutputPorts.horizontalHeaderItem(1)
-        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWidget", u"Device Name", None));
-        ___qtablewidgetitem2 = self.tableWidget_LocalOutputPorts.horizontalHeaderItem(2)
-        ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWidget", u"Network Name", None));
-        ___qtablewidgetitem3 = self.tableWidget_LocalOutputPorts.verticalHeaderItem(0)
-        ___qtablewidgetitem3.setText(QCoreApplication.translate("MainWidget", u"0", None));
-        ___qtablewidgetitem4 = self.tableWidget_LocalOutputPorts.verticalHeaderItem(1)
-        ___qtablewidgetitem4.setText(QCoreApplication.translate("MainWidget", u"1", None));
-        ___qtablewidgetitem5 = self.tableWidget_LocalOutputPorts.verticalHeaderItem(2)
-        ___qtablewidgetitem5.setText(QCoreApplication.translate("MainWidget", u"2", None));
-
-        __sortingEnabled = self.tableWidget_LocalOutputPorts.isSortingEnabled()
-        self.tableWidget_LocalOutputPorts.setSortingEnabled(False)
-        ___qtablewidgetitem6 = self.tableWidget_LocalOutputPorts.item(0, 0)
-        ___qtablewidgetitem6.setText(QCoreApplication.translate("MainWidget", u"\u2610", None));
-        ___qtablewidgetitem7 = self.tableWidget_LocalOutputPorts.item(0, 1)
-        ___qtablewidgetitem7.setText(QCoreApplication.translate("MainWidget", u"KAWAI CA-901 EP", None));
-        ___qtablewidgetitem8 = self.tableWidget_LocalOutputPorts.item(0, 2)
-        ___qtablewidgetitem8.setText(QCoreApplication.translate("MainWidget", u"CA-901 EP", None));
-        ___qtablewidgetitem9 = self.tableWidget_LocalOutputPorts.item(1, 0)
-        ___qtablewidgetitem9.setText(QCoreApplication.translate("MainWidget", u"\u2610", None));
-        ___qtablewidgetitem10 = self.tableWidget_LocalOutputPorts.item(1, 1)
-        ___qtablewidgetitem10.setText(QCoreApplication.translate("MainWidget", u"Arturia MINILAB mkII", None));
-        ___qtablewidgetitem11 = self.tableWidget_LocalOutputPorts.item(1, 2)
-        ___qtablewidgetitem11.setText(QCoreApplication.translate("MainWidget", u"MINILAB mkII", None));
-        ___qtablewidgetitem12 = self.tableWidget_LocalOutputPorts.item(2, 0)
-        ___qtablewidgetitem12.setText(QCoreApplication.translate("MainWidget", u"\u2610", None));
-        ___qtablewidgetitem13 = self.tableWidget_LocalOutputPorts.item(2, 1)
-        ___qtablewidgetitem13.setText(QCoreApplication.translate("MainWidget", u"Novation Launchpad Pro mk3", None));
-        ___qtablewidgetitem14 = self.tableWidget_LocalOutputPorts.item(2, 2)
-        ___qtablewidgetitem14.setText(QCoreApplication.translate("MainWidget", u"Launchpad Pro mk3", None));
-        self.tableWidget_LocalOutputPorts.setSortingEnabled(__sortingEnabled)
-
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWidget", u"Network Name", None));
         self.pushButton_LocalOutputPorts_SelectAll.setText(QCoreApplication.translate("MainWidget", u"Select &All", None))
-        self.pushButton_LocalOutputPorts_UnSelectAll.setText(QCoreApplication.translate("MainWidget", u"&Unselect All", None))
-        self.pushButton_LocalOutputPorts_Run.setText(QCoreApplication.translate("MainWidget", u"Run/Rerun", None))
-        self.pushButton_LocalOutputPorts_Stop.setText(QCoreApplication.translate("MainWidget", u"Stop", None))
+        self.pushButton_LocalOutputPorts_UnselectAll.setText(QCoreApplication.translate("MainWidget", u"&Unselect All", None))
+#if QT_CONFIG(tooltip)
+        self.pushButton_LocalOutputPorts_Refresh.setToolTip(QCoreApplication.translate("MainWidget", u"<html><head/><body><p>Refresh the list of output ports in case a new device is connected.</p></body></html>", None))
+#endif // QT_CONFIG(tooltip)
+        self.pushButton_LocalOutputPorts_Refresh.setText(QCoreApplication.translate("MainWidget", u"Re&fresh", None))
+        self.pushButton_LocalOutputPorts_Run.setText(QCoreApplication.translate("MainWidget", u"&Run", None))
+        self.pushButton_LocalOutputPorts_Stop.setText(QCoreApplication.translate("MainWidget", u"&Stop", None))
         self.radioButton.setText("")
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_OutgoingTraffic), QCoreApplication.translate("MainWidget", u"Outgoing Traffic", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_IncomingTraffic), QCoreApplication.translate("MainWidget", u"Incoming Traffic", None))
         self.groupBox_RTT.setTitle(QCoreApplication.translate("MainWidget", u"Round-Trip Times", None))
-        ___qtablewidgetitem15 = self.tableWidget_RTT.horizontalHeaderItem(0)
-        ___qtablewidgetitem15.setText(QCoreApplication.translate("MainWidget", u"Hostname", None));
-        ___qtablewidgetitem16 = self.tableWidget_RTT.horizontalHeaderItem(1)
-        ___qtablewidgetitem16.setText(QCoreApplication.translate("MainWidget", u"Minimum RTT", None));
-        ___qtablewidgetitem17 = self.tableWidget_RTT.horizontalHeaderItem(2)
-        ___qtablewidgetitem17.setText(QCoreApplication.translate("MainWidget", u"Maximum RTT", None));
-        ___qtablewidgetitem18 = self.tableWidget_RTT.horizontalHeaderItem(3)
-        ___qtablewidgetitem18.setText(QCoreApplication.translate("MainWidget", u"Average RTT", None));
-        ___qtablewidgetitem19 = self.tableWidget_RTT.horizontalHeaderItem(4)
-        ___qtablewidgetitem19.setText(QCoreApplication.translate("MainWidget", u"Lost Packets", None));
-        ___qtablewidgetitem20 = self.tableWidget_RTT.verticalHeaderItem(0)
-        ___qtablewidgetitem20.setText(QCoreApplication.translate("MainWidget", u"0", None));
+        ___qtablewidgetitem2 = self.tableWidget_RTT.horizontalHeaderItem(0)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWidget", u"Hostname", None));
+        ___qtablewidgetitem3 = self.tableWidget_RTT.horizontalHeaderItem(1)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("MainWidget", u"Minimum RTT", None));
+        ___qtablewidgetitem4 = self.tableWidget_RTT.horizontalHeaderItem(2)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("MainWidget", u"Maximum RTT", None));
+        ___qtablewidgetitem5 = self.tableWidget_RTT.horizontalHeaderItem(3)
+        ___qtablewidgetitem5.setText(QCoreApplication.translate("MainWidget", u"Average RTT", None));
+        ___qtablewidgetitem6 = self.tableWidget_RTT.horizontalHeaderItem(4)
+        ___qtablewidgetitem6.setText(QCoreApplication.translate("MainWidget", u"Lost Packets", None));
+        ___qtablewidgetitem7 = self.tableWidget_RTT.verticalHeaderItem(0)
+        ___qtablewidgetitem7.setText(QCoreApplication.translate("MainWidget", u"0", None));
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_Statistics), QCoreApplication.translate("MainWidget", u"Statitstics", None))
         self.textEdit_About.setHtml(QCoreApplication.translate("MainWidget", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
--- gui/main.py
+++ gui/main.py
@@ -1,26 +1,179 @@
-# Description: Main file for the GUI
+"""MIDI over LAN GUI client."""
 
-# To generate the python file from the ui file, run the following command:
-# pyside6-uic .\MainWidget.ui -o MainWidget.py
+# pylint: disable=line-too-long
+# pylint: disable=no-name-in-module
+# pylint: disable=no-member
+# pylint: disable=c-extension-no-member
+# pylint: disable=pointless-string-statement
+
+"""
+The GUI client is a simple "MIDI over LAN" application that allows you to
+select the MIDI output ports that you want to send over the network and the
+MIDI input ports that you want to receive from the network.
+
+The GUI has four sections:
+
+    1. Outgoing Traffic: A list of local MIDI output ports that you want to send
+       over the network.
+
+    2. Incoming Traffic: A list of MIDI input ports that you received from the
+       network and bind to local MIDI input ports.
+
+    3. Statistics: Round-trip time statistics between various hosts.
+
+    4. About: Information about the application.
+
+The GUI is implemented using the PySide6 library, which is a Python binding, and
+uses the Qt Designer to design the layout of the application.
+
+There are two worker processes that run in the background:
+    
+    1. The sender process sends MIDI messages over the network to the specified
+       hosts.
+    
+    2. The receiver process receives MIDI messages from the network and sends
+       them to the specified local MIDI input ports.
+
+The sender and receiver processes are implemented using the multiprocessing
+library. The entire communication between the sender and receiver processes is
+done using the multiprocessing.Queue class.
+
+Written by: Christoph Hänisch
+Last edited: 2025-02-14
+License: LGPL-3.0 (see LICENSE file)
+"""
 
 
 import sys
+from typing import List, Tuple
+
+import mido
 from PySide6 import QtWidgets
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QTableWidgetItem
+
 
 # Importing the midi_over_lan module
-sys.path.append("..")
+sys.path.append('.')
+sys.path.append('..')
 import midi_over_lan
+
+# To generate the python file from the ui file, run the following command:
+# pyside6-uic .\MainWidget.ui -o MainWidget.py
 from MainWidget import Ui_MainWidget
 
 
 class MainWidget(QtWidgets.QWidget, Ui_MainWidget):
+    """Main widget for the GUI."""
+
     def __init__(self):
+        """Initialize the main widget."""
         super().__init__()
         self.setupUi(self)
+        self.output_ports: List[Tuple[bool, str, str]] = []  # List of tuples (active, device_name, network_name)
+
+        # Setup the table widget.
+        self.tableWidget_LocalOutputPorts.clearSelection()
+        self.tableWidget_LocalOutputPorts.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        self.tableWidget_LocalOutputPorts.horizontalHeader().setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        self.tableWidget_LocalOutputPorts.setSortingEnabled(False)
+        self.tableWidget_LocalOutputPorts.itemClicked.connect(self.toggle_active_output_port)
+        self.tableWidget_LocalOutputPorts.itemChanged.connect(self.update_network_name)
+
+        # Connect the buttons to the functions.
+        self.pushButton_LocalOutputPorts_SelectAll.clicked.connect(self.select_all_output_ports)
+        self.pushButton_LocalOutputPorts_UnselectAll.clicked.connect(self.unselect_all_output_ports)
+        self.pushButton_LocalOutputPorts_Refresh.clicked.connect(self.refresh_output_ports)
+        self.pushButton_LocalOutputPorts_Run.clicked.connect(self.run)
+        self.pushButton_LocalOutputPorts_Stop.clicked.connect(self.stop)
+
+        # Add the output ports to the list.
+        self.refresh_output_ports()
 
 
-app = QtWidgets.QApplication(sys.argv)
+    def add_output_port(self, active: bool, device_name: str, network_name: str):
+        """Add an output port to the list of output ports."""
+        # Skip if the port is already in the list.
+        for port in self.output_ports:
+            if port[1] == device_name:
+                return
+        self.output_ports.append((active, device_name, network_name))
+        self.tableWidget_LocalOutputPorts.setRowCount(len(self.output_ports))
+        row = len(self.output_ports) - 1
+        item = QTableWidgetItem(device_name)
+        item.setCheckState(Qt.Checked if active else Qt.Unchecked)
+        item.setFlags(item.flags() & ~Qt.ItemIsEditable & ~Qt.ItemIsSelectable)
+        self.tableWidget_LocalOutputPorts.setItem(row, 0, item)
+        self.tableWidget_LocalOutputPorts.setItem(row, 1, QTableWidgetItem(network_name))
 
-widget = MainWidget()
-widget.show()
-app.exec()
+
+    def refresh_output_ports(self):
+        """Refresh the list of output ports."""
+        self.tableWidget_LocalOutputPorts.clearContents()
+        self.output_ports = []
+        for output_port in mido.get_output_names():
+            output_port = output_port.split(':')[0]
+            self.add_output_port(False, output_port, output_port)
+
+
+    def run(self):
+        """Start the sending process."""
+
+
+    def select_all_output_ports(self):
+        """Select all output ports."""
+        for row in range(self.tableWidget_LocalOutputPorts.rowCount()):
+            item = self.tableWidget_LocalOutputPorts.item(row, 0)
+            self.output_ports[row] = (True, self.output_ports[row][1], self.output_ports[row][2])
+            item.setCheckState(Qt.Checked)
+
+
+    def stop(self):
+        """Stop the sending process."""
+
+
+    def toggle_active_output_port(self, item: QTableWidgetItem):
+        """Toggle the active state of the output port."""
+        row = item.row()
+        column = item.column()
+        if column == 0:
+            device_name = self.output_ports[row][1]
+            network_name = self.output_ports[row][2]
+            item = self.tableWidget_LocalOutputPorts.item(row, 0)
+            if item.checkState() == Qt.Checked:
+                self.output_ports[row] = (False, device_name, network_name)
+                item.setCheckState(Qt.Unchecked)
+            else:
+                self.output_ports[row] = (True, device_name, network_name)
+                item.setCheckState(Qt.Checked)
+
+
+    def unselect_all_output_ports(self):
+        """Unselect all output ports."""
+        for row in range(self.tableWidget_LocalOutputPorts.rowCount()):
+            self.output_ports[row] = (False, self.output_ports[row][1], self.output_ports[row][2])
+            item = self.tableWidget_LocalOutputPorts.item(row, 0)
+            item.setCheckState(Qt.Unchecked)
+
+
+    def update_network_name(self, item: QTableWidgetItem):
+        """Update the network name of the output port."""
+        row = item.row()
+        column = item.column()
+        if column == 1:
+            active = self.output_ports[row][0]
+            device_name = self.output_ports[row][1]
+            network_name = item.text()
+            self.output_ports[row] = (active, device_name, network_name)
+
+
+def main():
+    """Main function."""
+    app = QtWidgets.QApplication(sys.argv)
+    widget = MainWidget()
+    widget.show()
+    app.exec()
+
+
+if __name__ == "__main__":
+    main()
