09adb87de848ece53755973c692d74e4649c08af
Christoph Hänisch <christoph.haenisch@rwth-aachen.de>
2025-02-17 23:30:41 +0100

    Added a wireshark dissector for the MIDI over LAN protocol.

---
+++ midi_over_lan/midi_over_lan_wireshark_dissector.lua
@@ -0,0 +1,59 @@
+-- Written by: Christoph Hänisch (with the help of ChatGPT)
+-- Last Change: 2025-02-17
+-- License: LGPL v3.0 or later (see LICENSE file)
+
+-- Define the MIDI over LAN protocol
+midi_lan_proto = Proto("MIDI_over_LAN", "MIDI over LAN Protocol")
+
+-- Define the protocol fields
+local f_header_mark = ProtoField.string("midilan.header_mark", "Header Mark")
+local f_version = ProtoField.uint8("midilan.version", "Version", base.DEC)
+local f_packet_type = ProtoField.uint8("midilan.packet_type", "Packet Type", base.DEC, {
+    [0] = "MIDI Message",
+    [1] = "Hello",
+    [2] = "Hello Reply"
+})
+local f_device_name_length = ProtoField.uint8("midilan.device_name_length", "Device Name Length", base.DEC)
+local f_device_name = ProtoField.string("midilan.device_name", "Device Name")
+local f_midi_data = ProtoField.bytes("midilan.midi_data", "MIDI Data")
+local f_num_device_names = ProtoField.uint8("midilan.num_device_names", "Number of Device Names", base.DEC)
+
+midi_lan_proto.fields = {f_header_mark, f_version, f_packet_type, f_device_name_length, f_device_name, f_midi_data, f_num_device_names}
+
+-- Create the dissector function
+function midi_lan_proto.dissector(buffer, pinfo, tree)
+    pinfo.cols.protocol = midi_lan_proto.name
+
+    local subtree = tree:add(midi_lan_proto, buffer(), "MIDI over LAN Protocol Data")
+
+    -- Parse the header
+    subtree:add(f_header_mark, buffer(0,4))
+    subtree:add(f_version, buffer(4,1))
+    local packet_type = buffer(5,1):uint()
+    subtree:add(f_packet_type, buffer(5,1))
+
+    -- Parse the payload based on packet type
+    if packet_type == 0 then
+        -- MIDI Message Packet
+        local device_name_length = buffer(6,1):uint()
+        subtree:add(f_device_name_length, buffer(6,1))
+        subtree:add(f_device_name, buffer(7, device_name_length))
+        subtree:add(f_midi_data, buffer(7 + device_name_length))
+    elseif packet_type == 1 or packet_type == 2 then
+        -- Hello or Hello Reply Packet
+        local num_device_names = buffer(6,1):uint()
+        subtree:add(f_num_device_names, buffer(6,1))
+        
+        local offset = 7
+        for i = 1, num_device_names do
+            local device_name_length = buffer(offset,1):uint()
+            subtree:add(f_device_name_length, buffer(offset,1))
+            subtree:add(f_device_name, buffer(offset + 1, device_name_length))
+            offset = offset + 1 + device_name_length
+        end
+    end
+end
+
+-- Register your protocol on the specified UDP port
+local udp_port = DissectorTable.get("udp.port")
+udp_port:add(56129, midi_lan_proto)
